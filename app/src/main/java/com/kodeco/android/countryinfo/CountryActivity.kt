c62fc2ec8060c45303d0f8390af5ec95b637279e
Anaz Abdul Azeez <duoanaz007@gmail.com>
2024-02-25 11:12:55 -0500

    TIRAMISU Android 33 check for deprecation warning

--- app/src/main/java/com/kodeco/android/countryinfo/CountryActivity.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/CountryActivity.kt
@@ -2,9 +2,11 @@ package com.kodeco.android.countryinfo
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -16,8 +18,14 @@ import com.kodeco.android.countryinfo.ui.theme.MyApplicationTheme
 
 class CountryActivity : AppCompatActivity() {
 
-    private val country: Country by lazy {
-        intent?.getParcelableExtra(COUNTRY_ID)!!
+
+    private val country by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(COUNTRY_ID, Country::class.java)
+        }
+        else{
+            intent?.getParcelableExtra(COUNTRY_ID)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +33,7 @@ class CountryActivity : AppCompatActivity() {
 
         setContent {
             MyApplicationTheme {
-                CountryDetailsScreen(country)
+                country?.let { CountryDetailsScreen(it) }
             }
         }
     }
