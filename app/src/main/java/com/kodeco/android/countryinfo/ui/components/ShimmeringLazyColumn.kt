26f25037240bafd0aaac4599cdd45954fbbbcd5b
Anaz Abdul Azeez <duoanaz007@gmail.com>
2024-04-06 23:10:48 -0400

    Removing unused imports

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/ShimmeringLazyColumn.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/ShimmeringLazyColumn.kt
@@ -5,22 +5,10 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.facebook.shimmer.Shimmer
-import com.facebook.shimmer.ShimmerDrawable
 
 @Composable
 fun shimmerBrush(showShimmer: Boolean = true,targetValue:Float = 1000f): Brush {
